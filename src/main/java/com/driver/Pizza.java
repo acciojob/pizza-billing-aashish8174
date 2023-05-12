7735000d09a3e672a0c65393224e67e14dca919c
aashish kumar yadav <aashish8174@gmail.com>
2023-05-12 18:57:40 +0530

    every thing implemented as per instruction

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,13 +1,29 @@
 package com.driver;
 
 public class Pizza {
-
-    private int price;
-    private Boolean isVeg;
+    private final int price;
+    boolean isVeg;
     private String bill;
+    private boolean extraCheese = false;
+    private int extraCheesePrice;
+    private final boolean extraTopping = false;
+    int toppingPrice;
+    boolean takeAway = false;
+    int bagPrise = 20;
+    private int totalPrice ;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+            this.price = 300;
+            this.toppingPrice =70;
+        }
+        else {
+            this.price = 400;
+            this.toppingPrice = 120;
+        }
+        totalPrice = price;
         // your code goes here
     }
 
@@ -16,19 +32,45 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
+        if(!this.extraCheese){ // no extra cheese
+            this.extraCheese = true;
+            extraCheesePrice = 80;
+            totalPrice+= extraCheesePrice;
+        }
         // your code goes here
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        // your code goes her
+        if(!extraTopping) {
+            totalPrice += toppingPrice;
+            boolean extraTopping = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!takeAway){
+            takeAway=true;
+            this.totalPrice+=this.bagPrise;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        StringBuilder sb = new StringBuilder();
+        sb.append("Base Price Of The Pizza:").append(price).append("/n");
+        if(extraCheese){
+            sb.append("Extra Cheese Added:").append(extraCheesePrice).append("/n");
+        }
+        if(extraTopping){
+            sb.append("Extra Toppings Added:").append(toppingPrice).append("/n");
+        }
+        if(takeAway){
+            sb.append("Paperbag Added:").append(bagPrise).append("/n");
+        }
+        sb.append("Total Price:").append(totalPrice).append("/n");
+        this.bill = sb.toString();
         return this.bill;
     }
 }
